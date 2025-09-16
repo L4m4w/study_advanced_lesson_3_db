f70e045280c8b2507ec1f2e48b9e2f422f876a6c
L4m4w <l4p41@yandex.com>
2025-09-16 19:41:44 +0300

    init commit

---
+++ qa_study_lesson_1/first_task_api_tests.py
@@ -0,0 +1,9 @@
+import json
+
+import requests
+from jsonschema import validate
+
+"""
+1. Разработать несколько API-автотестов на https://reqres.in
+(если обучались на основном курсе python - можно взять код автотестов из домашнего задания)
+"""
