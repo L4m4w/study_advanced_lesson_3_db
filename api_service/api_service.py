7f8e40dcb5a9a7526914790507aa6f89f02ba71d
L4m4w <l4p41@yandex.com>
2025-09-19 23:30:08 +0300

    added pagination + smoke tests

--- api_service/api_service.py
+++ api_service/api_service.py
@@ -1,11 +1,13 @@
 import json
 
 import uvicorn
+from fastapi.params import Depends
+from fastapi_pagination import Params, paginate
 from starlette.responses import JSONResponse
 from fastapi import FastAPI, Header, HTTPException
 from http import HTTPStatus
 
-from helpers import PROJECT_ROOT
+from helpers import PROJECT_ROOT, get_custom_params
 from models.AppStatus import AppStatus
 from models.Users import User
 
@@ -17,8 +19,12 @@ users: list[User] = []
 def status() -> AppStatus:
     return AppStatus(users=bool(users))
 
+@app.get("/api/status", status_code=HTTPStatus.OK)
+def status() -> dict[str, str]:
+    return {"status": "healthy"}
+
 @app.get("/api/users/{id}")
-def get_user_by_id(id: int):
+def get_user_by_id(id: int) -> User:
     if id < 1:
         raise HTTPException(status_code=HTTPStatus.UNPROCESSABLE_ENTITY, detail=("Invalid user id - ", id))
     if id > len(users):
@@ -26,22 +32,25 @@ def get_user_by_id(id: int):
     return users[id - 1]
 
 @app.get("/api/users")
-def get_users(page='1', per_page=None, x_api_key = Header(default=None)):
-    if x_api_key  != "reqres-free-v1":
+def get_users( x_api_key: str = Header(default=None), params: Params = Depends(get_custom_params)) -> dict:
+    if x_api_key != "reqres-free-v1":
         raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail={"error": "Missing API key"})
-    else:
-        response_message = {
-            "page": page,
-            "per_page": per_page,
-            "total": 3,
-            "total_pages": 2,
-            "data": users,
-            "support": {
-                "url": "https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral",
-                "text": "Tired of writing endless social media content? Let Content Caddy generate it for you."
-            }
+
+    paginated_data = paginate(users, params)
+
+    response_message = {
+        "page": params.page,
+        "per_page": params.size,
+        "total": len(users),
+        "total_pages": (len(users) + params.size - 1) // params.size,
+        "data": paginated_data.items,
+        "support": {
+            "url": "https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral",
+            "text": "Tired of writing endless social media content? Let Content Caddy generate it for you."
         }
-        return response_message
+    }
+    return response_message
+
 
 if __name__ == "__main__":
     with open(file=PROJECT_ROOT / "users.json") as f:
--- api_service/helpers.py
+++ api_service/helpers.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 
+from fastapi_pagination.bases import AbstractParams, RawParams
+from fastapi_pagination.default import Page, Params
+
+
 def find_project_root():
     current = Path(__file__).parent
     while not (current / ".git").exists() and not (current / "pyproject.toml").exists():
@@ -9,3 +13,6 @@ def find_project_root():
     return current
 
 PROJECT_ROOT = find_project_root()
+
+def get_custom_params(page: int = 1, size: int = 12) -> Params:
+    return Params(page=page, size=size)
